9376d203a7c440443a576b5bd1a8854dd0fa3609
kongsubin <qls0919@naver.com>
2019-04-08 16:45:44 +0900

    create menu.c

--- menu.c
+++ menu.c
@@ -4,17 +4,18 @@ int ask_menu(int is_login){
 #ifdef DEBUG_MODE
         printf("DEBUG: int ask_menu()\n");
 #endif
-  char menu[20];
+  char menu[200];
 // 파라미터 : 로그인여부 (0 No, 1Yes)
 // 리턴값 : 선택한 메뉴번호
 // 메뉴번호 : 0 exit, 1 join, 2 login, 3 logout, 4 list
  //int menu;
+	
   while(1){
     if(is_login==0 || is_login==-1){
         printf("> ");
-        scanf("%s", menu);
-
-        if(!strcmp("list", menu))
+	scanf("%s", menu);
+	
+	if(!strcmp("list", menu))
             return LIST_4;
         else if(!strcmp("join", menu))
             return JOIN_1;
@@ -28,7 +29,8 @@ int ask_menu(int is_login){
 
     else{
         printf("# ");
-        scanf("%s", menu);
+	fgets(menu, sizeof(menu), stdin);
+	menu[strlen(menu)-1] = 0;
 
         if(!strcmp("logout", menu))
             return LOGOUT_3;
